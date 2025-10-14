ce2013ba33ee140f28943baa9facf6c5a5f6669d
jairolessa <jairo.rafaelcarvalho@gmail.com>
2025-10-14 15:13:04 -0300

    Criando model user e atualizando o model responsible

---
+++ demo/src/main/java/com/projetoTEA5/demo/model/User.java
@@ -0,0 +1,72 @@
+package com.projetoTEA5.demo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String profile;
+
+    private Boolean active = true;
+
+    public User() {
+    }
+
+    public User(String email, String password, String profile, Boolean active) {
+        this.email = email;
+        this.password = password;
+        this.profile = profile;
+        this.active = active;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getProfile() {
+        return profile;
+    }
+
+    public void setProfile(String profile) {
+        this.profile = profile;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+}
